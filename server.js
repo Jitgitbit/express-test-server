f592246a6e66a910bf1020cc6b313134b528d8e2
Jitgitbit <rowowolf@gmail.com>
2020-02-04 16:57:44 +0100

    added process.env.PORT

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT || 3789;
+
 app.get('/', (req, res)=>{
   console.log('hi there');
   res.send('hello world')
 })
-app.listen(3789, () => {
+app.listen(port, () => {
   console.log('server started')
 });
