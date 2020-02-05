c83d2dd15035f42df603da7f75c39802b6c80a8f
Jitgitbit <rowowolf@gmail.com>
2020-02-05 10:00:38 +0100

    kjb

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3789;
 
 // const connection = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:postgres/5432');
-const connection = new Sequelize(process.env.DATABASE_URL || 'https://express-server-app-exercise.herokuapp.com/');
+const connection = new Sequelize(process.env.DATABASE_URL || 'https://express-server-app-exercise.herokuapp.com/5432');
 
 app.get('/', (req, res)=>{
   console.log('hi there');
